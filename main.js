cff59cb357dec0c0baadcb553619b16fa24ca635
Chai Phonbopit <Phonbopit@gmail.com>
2016-10-14 01:10:54 +0700

    add prefix

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 'use strict';
 
 (function() {
-  document.querySelector('html').style.filter = 'grayscale(1)';
+  var html = document.querySelector('html');
+  html.style.webkitFilter = 'grayscale(1)';
+  html.style.filter = 'grayscale(1)';
 })();
